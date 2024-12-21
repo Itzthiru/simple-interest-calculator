4aad5722f617687a58da961526abab6b68d40a5b
THIRUMURUGAN B <83574560+Itzthiru@users.noreply.github.com>
2024-12-21 16:54:36 +0530

    Updated simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+# Simple Interest Calculator
+
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 principal rate time"
+  exit 1
+fi
+
+principal=$1
+rate=$2
+time=$3
+
+interest=$(echo "$principal * $rate * $time / 100" | bc -l)
+echo "The simple interest is: $interest"
